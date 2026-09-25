39c37683db4989be999d535645b6ba9475b71e11
Swapna <swapnavp007@gmail.com>
2026-09-25 11:35:37 +0530

    Day1 Assignment

--- src/Daywise_Assignment/Day1.java
+++ src/Daywise_Assignment/Day1.java
@@ -3,8 +3,36 @@ package Daywise_Assignment;
 public class Day1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//What is the output?
+				int a=10, b=3;
+				System.out.println(a/b); // output : 3
+				System.out.println(a%b); // output : 1
+			//Find x, y and z:
+				int x=5;  // 5+1 = 6 +1 = 7
+				int y=x++; // output : 5
+				int z=++x; // output : 7
+			//What is d?
+				double d = 10 / 4; // output : 2.5
+				
+				// Fix:
+				float price = 99.99F; // output : 99.99f
+				
+				//Predict:
+				a=10; // 10+1 = 11 +1 =12 
+				System.out.println(a++ + ++a); // output : 10 + 12 = 22
+				
+				//Write a program using ?: to print PASS/FAIL.
+				
+				//Build a salary calculator:
+				int basic = 50000;
+				int bonus = 5000;
+				 tax = 10%;
+				// gross = basic + bonus 
+						int gross = 55000;
+						
+				//print gross, tax and net salary.
+						System.out.println(gross);
+						
 	}
 
 }
