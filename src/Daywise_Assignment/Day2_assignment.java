e237fe4486539b723d57bd2a5f4720731989ec79
Swapna <swapnavp007@gmail.com>
2026-09-26 23:44:10 +0530

    Day2 Assignment

---
+++ src/Daywise_Assignment/Day2_assignment.java
@@ -0,0 +1,54 @@
+package Daywise_Assignment;
+
+public class Day2_assignment {
+	public static void main(String[]args) {
+		
+	//BASIC:
+		//Create variables for employee name, ID, department, salary and active status. Print a neat report.
+		/* String employee_name = "Swapna V";
+		 String department = "Software Testing";
+		 int salary = 60000;
+		 boolean active_status = false;
+		 System.out.println("I am " + employee_name + " " + "working as " + department + " "+ "with salary of " + salary+ " " + "and current working status " + active_status);
+		 
+		 //Create a product with price and quantity. Calculate subtotal, 18% tax and final total.
+		 String product = "bottles";
+		 double price = 99.99;
+		 int quantity = 8;
+	// total = price * quantity;
+		 double subtotal = price * quantity;
+		 System.out.println("The subtotal amount is" + subtotal);
+		// Tax = 18% of subtotal
+		 double tax = 0.18 * subtotal;
+		 System.out.println("Tax for the product is " + tax);
+		 //final amount = subtotal * tax
+		double  final_Amount = subtotal * tax;
+		 System.out.println("Final Amount for " + quantity + " " + product+ " "+ "is " + final_Amount  ); */
+		 
+		 //Read an integer and print whether it is positive, negative or zero.
+		 
+		 
+		 //Check whether a number is divisible by both 3 and 5 using logical operators.
+		 
+		 
+		//Create two integers and print sum, difference, product, quotient and remainder.
+		 int a = 85;
+		 int b= 72;
+		int  sum = a+b;
+		int diff = a-b;
+		int product = a*b;
+		int quotient = a/b;
+		int remainder = a%b;
+		 System.out.println("The sum of two intergers is: " + sum);
+		 System.out.println("The difference between two intergers is: " +diff );
+		 System.out.println("Product of two numbers is: " + product);
+		 System.out.println("The sum of two intergers is: " + quotient);
+		 System.out.println("The sum of two intergers is: " + remainder);
+		 
+		 
+		 
+		 
+		
+	}
+
+}
